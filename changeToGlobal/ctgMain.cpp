0394877b6d494a5df595048e76c6f49ea5fdf804
Kenshin Himura <kenshin.thebattosai@live.com>
2012-12-29 00:48:20 +0530

    Global Coordinate Mapper Update
    Attempted a global coordinate mapper using custom Matrix class and
    reading from .2dparse files. Force Closes due to insufficient memory
    when asked to allocate memory for an array of 1000 elements of type
    double. reset array sizes to 1 and wondering what to do.

--- changeToGlobal/ctgMain.cpp
+++ changeToGlobal/ctgMain.cpp
@@ -2,8 +2,13 @@
 
 void main()
 {
-	/*fstream fileToOpen;
+	fstream fileToOpen;
 	string fileName;
+	double globalX[1];
+	double globalY[1];
+	double globalT[1];
+	Matrix *matrixStack[1][541];
+	double tempX,tempY;
 	cout<<"Enter file name (without extension .2dparse):";
 	cin>>fileName;
 	fileToOpen.open(fileName+".2dparse",ios::in);
@@ -12,23 +17,40 @@ void main()
 		cout<<"ERROR OPENING FILE.";
 		_getch();
 		exit(0);
-	}*/
-	//First testing for normal x,y
-	matrixA=new Matrix(3,3);
-	matrixB=new Matrix(3,3);
-	matrixC=new Matrix(3,3);
-	matrixD=new Matrix(3,3);
-	for(int i=0;i<3;i++)
-		for(int j=0;j<3;j++)
-			matrixA->setCell(i,j,((i*3)+j+1));
-	matrixA->print();
-	for(int i=0;i<3;i++)
-		for(int j=0;j<3;j++)
-			matrixB->setCell(i,j,((i*3)+j+1));
-	matrixB->print();
-	matrixC->setMatrix(*(matrixA->operator+(*matrixB)));
-	matrixD->setMatrix(*(matrixA->operator*(*matrixB)));
-	matrixC->print();
-	matrixD->print();
+	}
+	string stringArray[1086];
+	for(int i=0,j=0;((!fileToOpen.eof())&&(j!=1000));i++)
+	{
+		if(i==0)
+			fileToOpen>>stringArray[i];
+		else if(i==1)
+		{
+			stringstream globX(stringArray[i]);
+			globX>>globalX[j];
+			fileToOpen>>stringArray[++i];
+			stringstream globY(stringArray[i]);
+			globY>>globalY[j];
+			fileToOpen>>stringArray[++i];
+			stringstream globT(stringArray[i]);
+			globT>>globalT[j];
+		}
+		else if(i>3)
+		{
+			fileToOpen>>stringArray[i];
+			stringstream relX(stringArray[i]);
+			relX>>tempX;
+			matrixStack[j][int((i-4)/2)]->setCell(0,0,tempX);
+			fileToOpen>>stringArray[++i];
+			stringstream relY(stringArray[i]);
+			relY>>tempY;
+			matrixStack[j][int((i-4)/2)]->setCell(1,0,tempY);
+		}
+		if(i==1085)
+		{
+			break;
+		}
+	}
+	for(int i=0;i<541;i++)
+		matrixStack[0][i]->print();
 	_getch();
 }
