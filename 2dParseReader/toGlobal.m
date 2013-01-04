702018cb97fa03c58f0840830d886f7bd64a0ce6
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-04 13:54:01 +0530

    toGlobal CleanUp Update
    Used identity matrix function to remove certain 1 value assignments to
    elements in matrices of the toGlobal function.

--- 2dParseReader/toGlobal.m
+++ 2dParseReader/toGlobal.m
@@ -1,27 +1,19 @@
 function [G] = toGlobal(LP,GP,CP)
 %Returns a global pose based on local pose, global pose and a correction
 %pose
-    mulA=zeros(3,3);
-    mulB=zeros(3,3);
-    mulC=zeros(3,3);
-    mulD=zeros(3,3);
-    mulB(3,3)=1;
+    mulA=eye(3,3);
+    mulB=mulA;
+    mulC=mulA;
+    mulD=mulA;
     mulB(1,3)=GP(1,1);
     mulB(2,3)=GP(1,2);
-    mulB(1,1)=1;
-    mulB(2,2)=1;
-    mulC(3,3)=1;
     mulC(1,3)=CP(1,1);
     mulC(2,3)=CP(1,2);
-    mulC(1,1)=1;
-    mulC(2,2)=1;
-    mulA(3,3)=1;
     theta=GP(1,3)/180*pi;
     mulA(1,1)=cos(theta);
     mulA(2,1)=sin(theta);
     mulA(1,2)=-mulA(2,1);
     mulA(2,2)=mulA(1,1);
-    mulA(3,3)=1;
     beta=CP(1,3)/180*pi;
     mulD(1,1)=cos(beta);
     mulD(2,1)=sin(beta);
