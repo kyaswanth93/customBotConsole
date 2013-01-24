a262f78ffecf256b81956f0134d992db95b9cd78
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-24 17:51:49 +0530

    lsFit and calcWindow Update

--- 2dParseReader/getUnion.m
+++ 2dParseReader/getUnion.m
@@ -1,11 +1,11 @@
 %gives the union of n scans and plots it
 function getUnion(N,scan)
-globalXY=zeros(1,2);
-figure(1);
-for i=1:N
-    globalXY=union(globalXY,scan(i).globalPoses(1:2,:)','rows');
-end;
-scatter(globalXY(:,1),globalXY(:,2));
-figure(2);
-plot(globalXY(:,1),globalXY(:,2));
+    globalXY=zeros(1,2);
+    figure(1);
+    for i=1:N
+        globalXY=union(globalXY,scan(i).globalPoses(1:2,:)','rows');
+    end;
+    scatter(globalXY(:,1),globalXY(:,2));
+    figure(2);
+    plot(globalXY(:,1),globalXY(:,2));
 end
